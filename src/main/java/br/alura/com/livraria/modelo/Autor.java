e52ae3c0226dbfc72ff24f9ee9be3194e76d08e0
Sergio Uno <unosergios@gmail.com>
2021-10-05 20:56:43 -0300

    Criacao de tab via JPA e mapeamento das entidades Livro e Autor

--- src/main/java/br/alura/com/livraria/modelo/Autor.java
+++ src/main/java/br/alura/com/livraria/modelo/Autor.java
@@ -2,6 +2,12 @@ package br.alura.com.livraria.modelo;
 	
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +16,13 @@ import lombok.Setter;
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name="autores")
 public class Autor {
 
-
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 		private String nome;
 		private String email;
 		private LocalDate dataNascimento;
